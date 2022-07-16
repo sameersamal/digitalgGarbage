8f5d78df10d70bee9c8aecffa0259881a869a5a7
Sameer Panchal <sameer.samtalu@gmail.com>
2022-07-16 06:53:29 +0530

    cm-16072022

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { BinComponent } from './bin/bin.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { LoginComponent } from './login/login.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  { path: 'login', component: LoginComponent },
+  { path: 'bin', component: BinComponent },
+  { path: 'dashboard', component: DashboardComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+export const routingComponents = [LoginComponent, BinComponent, DashboardComponent ];
