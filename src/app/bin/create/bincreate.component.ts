048e5e2ba5069b63c8419ea06c8a410ba99ba6c6
Sameer Panchal <sameer.samtalu@gmail.com>
2022-08-10 09:14:46 +0530

    bincreate_validation_08102022

--- src/app/bin/create/bincreate.component.ts
+++ src/app/bin/create/bincreate.component.ts
@@ -7,7 +7,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BinCreateComponent implements OnInit {
 
-  constructor() { }
+  createbin(form: { value: any; }) {
+    console.log (form.value);
+  }
 
   ngOnInit(): void {
   }
